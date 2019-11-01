68a86f562f2c5f4283e65b3595ae11bee756f0f3
Dustin Beckett <dustin.beckett23@gmail.com>
2019-10-31 22:56:32 -0400

    tracking needed files

---
+++ Source/Nero/NeroGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeroGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANeroGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANeroGameMode();
+};
+
+
+
